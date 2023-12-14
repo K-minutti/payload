4e3fbb8c8f884ac4ef374d05cb04c638da6b2f05
Kevin Minutti <minutti70@gmail.com>
2023-12-14 10:35:58 -0500

    explore templates

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Payload
 A module for payload mocking
+
+Generate fake data for fields in json
+
+```json
+{
+    "timestamp": "{{$timestamp}}",
+    "id": "{{$guid}}",
+    "data": {
+        "name": "{{$randomFirstName}} {{$randomLastName}}",
+        "date": "{{$randomDatePast}}",
+    }
+}
+```
+
+```json
+{
+    "timestamp": "{{.Timestamp}}",
+    "id": "{{.GUID}}",
+    "data": {
+        "name": "{{.RandomFirstName}} {{.RandomLastName}}",
+        "date": "{{.RandomDatePast}}",
+    }
+}
+```
+
+use in tests
+
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package payload
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
 
+type Data struct {
+	GUID            string
+	Timestamp       string
+	RandomFirstName string
+	RandomLastName  string
+	RandomDatePast  string
+}
 
 func Hello(name string) string {
 	msg := fmt.Sprintf("Hi, %v. Hello how are you?", name)
 	return msg
+}
+
+func Parse(body string) {
+	d := Data{
+		GUID: "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
+		Timestamp: "1562757109",
+		RandomFirstName: "Heaven",
+		RandomLastName: "Earth",
+		RandomDatePast: "Thu Jun 13 2019 03:08:43 GMT+0530",
+	}
+
+	t, err := template.New("x").Parse(body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(os.Stdout, d)
+	if err != nil {
+		panic(err)
+	}
 }
