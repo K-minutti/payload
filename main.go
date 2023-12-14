9ddc2eafc8a0b4ab86db8112e9aa2f9844697234
Kevin Minutti <minutti70@gmail.com>
2023-12-14 16:43:16 -0500

    return executed template as string

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package payload
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"text/template"
@@ -19,7 +20,32 @@ func Hello(name string) string {
 	return msg
 }
 
-func Parse(body string) {
+func Parse(body string) string {
+	d := Data{
+		GUID: "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
+		Timestamp: "1562757109",
+		RandomFirstName: "Heaven",
+		RandomLastName: "Earth",
+		RandomDatePast: "Thu Jun 13 2019 03:08:43 GMT+0530",
+	}
+
+	t, err := template.New("x").Parse(body)
+	if err != nil {
+		panic(err)
+	}
+
+	var r bytes.Buffer
+	
+	err = t.Execute(&r, d)
+	if err != nil {
+		panic(err)
+	}
+
+	s := r.String()
+	return s
+}
+
+func ParseDisplay(body string) {
 	d := Data{
 		GUID: "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
 		Timestamp: "1562757109",
