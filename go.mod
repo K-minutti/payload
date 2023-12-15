6404f702134f82b1506129b9902cea5a0b25f600
Kevin Minutti <minutti70@gmail.com>
2023-12-15 11:34:32 -0500

    add faker

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/K-minutti/payload
 
 go 1.20
+
+require github.com/jaswdr/faker v1.19.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package payload
 import (
 	"bytes"
 	"fmt"
+	"github.com/jaswdr/faker"
 	"os"
 	"text/template"
 )
@@ -21,12 +22,13 @@ func Hello(name string) string {
 }
 
 func Parse(body string) string {
+	f := faker.New()
 	d := Data{
-		GUID: "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
-		Timestamp: "1562757109",
-		RandomFirstName: "Heaven",
-		RandomLastName: "Earth",
-		RandomDatePast: "Thu Jun 13 2019 03:08:43 GMT+0530",
+		GUID:            "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
+		Timestamp:       "1562757109",
+		RandomFirstName: f.Person().FirstName(),
+		RandomLastName:  f.Person().LastName(),
+		RandomDatePast:  "Thu Jun 13 2019 03:08:43 GMT+0530",
 	}
 
 	t, err := template.New("x").Parse(body)
@@ -35,7 +37,7 @@ func Parse(body string) string {
 	}
 
 	var r bytes.Buffer
-	
+
 	err = t.Execute(&r, d)
 	if err != nil {
 		panic(err)
@@ -47,11 +49,11 @@ func Parse(body string) string {
 
 func ParseDisplay(body string) {
 	d := Data{
-		GUID: "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
-		Timestamp: "1562757109",
+		GUID:            "611c2e81-2ccb-42d8-9ddc-2d0bfa65c1b4",
+		Timestamp:       "1562757109",
 		RandomFirstName: "Heaven",
-		RandomLastName: "Earth",
-		RandomDatePast: "Thu Jun 13 2019 03:08:43 GMT+0530",
+		RandomLastName:  "Earth",
+		RandomDatePast:  "Thu Jun 13 2019 03:08:43 GMT+0530",
 	}
 
 	t, err := template.New("x").Parse(body)
